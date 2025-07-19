98f5f5b87e67aa77cb4e184231349fba8cac6ccd
tristan vaber <tristanvaber94@gmail.com>
2025-07-19 14:13:45 -0400

    New update for my project

--- scripts/index.js
+++ scripts/index.js
@@ -2,7 +2,7 @@ const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 
-const newPostBtn = document.querySelector(".profile__new-post-btn");
+const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostModal = document.querySelector("#new-post-modal");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 
